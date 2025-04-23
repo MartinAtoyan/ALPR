fbe309e13adaa54bcf9039903065bc2043ed1c23
MartinAtoyan <martinwork31@gmail.com>
2025-04-23 17:34:17 +0400

    thresholding(binary, binary_inv, truncated, to zero, to zero inv)

---
+++ ALPR-96/threshold.py
@@ -0,0 +1,35 @@
+import cv2
+import numpy
+
+image = cv2.imread("input_image.jpg", cv2.IMREAD_GRAYSCALE)
+
+thresh = 127
+maxValue = 255
+
+# All values less than thresh are invisible
+# _, threshold = cv2.threshold(image, thresh, maxValue, cv2.THRESH_BINARY)
+# concatenated = numpy.concatenate((image, threshold), axis=1)
+# cv2.imshow("Original and threshold", concatenated)
+
+# Inverse of first threshold
+# _, threshold_inv = cv2.threshold(image, thresh, maxValue, cv2.THRESH_BINARY_INV)
+# concatenated = numpy.concatenate((image, threshold_inv), axis=1)
+# cv2.imshow("Original and threshold", concatenated)
+
+# All values greater than thresh are set to thresh value, all values less than thresh are unchanged.
+# _, threshold_trunc = cv2.threshold(image, thresh, maxValue, cv2.THRESH_TRUNC)
+# concatenated_trunc = numpy.concatenate((image, threshold_trunc), axis=1)
+# cv2.imshow("Original and threshold", concatenated_trunc)
+
+# Visible numbers only greater thresh
+# _, threshold_zero = cv2.threshold(image, thresh, maxValue, cv2.THRESH_TOZERO)
+# concatenated_trunc = numpy.concatenate((image, threshold_zero), axis=1)
+# cv2.imshow("Original and threshold", concatenated_trunc)
+
+# Visible only numbers which thresh border greater than thresh, (ketgic)
+# _, threshold_inv_zero = cv2.threshold(image, thresh, maxValue, cv2.THRESH_TOZERO_INV)
+# concatenated_trunc = numpy.concatenate((image, threshold_inv_zero), axis=1)
+# cv2.imshow("Original and threshold", concatenated_trunc)
+
+cv2.waitKey()
+cv2.destroyAllWindows()
